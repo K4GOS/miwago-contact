255134f73b02a5985aee0fca8d8c0db8a6d7e6e8
K4GOS <come.paya@gmail.com>
2024-03-09 18:26:56 +0100

    Finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./icon.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className="App"
+      style={{ display: "flex", justifyContent: "center", height: "100vh" }}
+    >
+      <header className="App-header"></header>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          gap: "20px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "20px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <img
+            src={logo}
+            style={{ width: "150px", borderRadius: "8px" }}
+            alt="logo"
+          />
+          <div>
+            <p style={{ color: "white", fontSize: "40px" }}>Miwago</p>
+          </div>
+        </div>
+        <p style={{ color: "white", fontSize: "20px" }}>
+          To delete your data from your Miwago account, send an email to :{" "}
+          <a
+            style={{
+              cursor: "pointer",
+              textDecoration: "underline",
+              color: "white",
+            }}
+            href="contactmiwago@gmail.com"
+          >
+            contactmiwago@gmail.com
+          </a>{" "}
+          containing your username.
+        </p>
+        <p style={{ color: "white", fontSize: "20px" }}>
+          All your account data will be permanently deleted, namely:
+        </p>
+        <ul>
+          <li style={{ color: "white", fontSize: "20px" }}>
+            Your kanji scores
+          </li>
+          <li style={{ color: "white", fontSize: "20px" }}>Your Username</li>
+          <li style={{ color: "white", fontSize: "20px" }}>
+            Your revisions and posts to watch later
+          </li>
+          <li style={{ color: "white", fontSize: "20px" }}>Your JLPT Level</li>
+        </ul>
+      </div>
     </div>
   );
 }
